1ed07f75745d4b29cbb64052e6a7c45d5d6eac3b
Josh Geden <joshgeden10@gmail.com>
2021-05-08 21:25:22 -0400

    initial commit

---
+++ app.py
@@ -0,0 +1,21 @@
+from flask import Flask, render_template, send_from_directory
+import os
+app = Flask(__name__)
+
+@app.route('/')
+def index_page():
+    return render_template('index.html')
+
+@app.route('/projects')
+def projects_page():
+    return render_template('projects.html')
+
+@app.route('/resume')
+def resume_page():
+    directory = os.path.abspath(os.getcwd())
+    filepath = directory + '/static/files/'
+    return send_from_directory(filepath, 'Joshua_Geden_Resume.pdf')
+
+@app.route('/contact')
+def contact_page():
+    return render_template('contact.html')
