efe2f732fe7622cf4204233a865c5949ac5c8ad7
Josh Geden <joshgeden10@gmail.com>
2021-05-09 22:05:33 -0400

    update website

--- app.py
+++ app.py
@@ -11,8 +11,16 @@ def index_page():
 def projects_page():
     return render_template('projects.html')
 
+@app.route('/skills')
+def skils_page():
+    return render_template('skills.html')
+
 @app.route('/resume')
 def resume_page():
+    return render_template('resume.html')
+
+@app.route('/resumefile')
+def resume():
     directory = os.path.abspath(os.getcwd())
     filepath = directory + '/static/files/'
     return send_from_directory(filepath, 'Joshua_Geden_Resume.pdf')
