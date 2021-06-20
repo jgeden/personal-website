320e2032ab72ba7989dca7a56016aaf610ecf9ed
Josh Geden <joshgeden10@gmail.com>
2021-06-20 15:52:06 -0400

    initial commit for v3

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,183 +1,91 @@
-import * as React from "react"
+import React from 'react';
+import ReactFullpage from '@fullpage/react-fullpage';
 
-// styles
-const pageStyles = {
-  color: "#232129",
-  padding: 96,
-  fontFamily: "-apple-system, Roboto, sans-serif, serif",
-}
-const headingStyles = {
-  marginTop: 0,
-  marginBottom: 64,
-  maxWidth: 320,
-}
-const headingAccentStyles = {
-  color: "#663399",
-}
-const paragraphStyles = {
-  marginBottom: 48,
-}
-const codeStyles = {
-  color: "#8A6534",
-  padding: 4,
-  backgroundColor: "#FFF4DB",
-  fontSize: "1.25rem",
-  borderRadius: 4,
-}
-const listStyles = {
-  marginBottom: 96,
-  paddingLeft: 0,
-}
-const listItemStyles = {
-  fontWeight: 300,
-  fontSize: 24,
-  maxWidth: 560,
-  marginBottom: 30,
-}
+import '../styles/style.css'
 
-const linkStyle = {
-  color: "#8954A8",
-  fontWeight: "bold",
-  fontSize: 16,
-  verticalAlign: "5%",
-}
+const Body = () => (
+  <ReactFullpage
+    //fullpage options
+    licenseKey={'YOUR_KEY_HERE'}
+    scrollingSpeed={1000} /* Options here */
+    autoScrolling={true}
+    navigation={true}
+    anchors={['section1', 'section2', 'section3', 'section4']}
+    navigationTooltips={['Home', 'About', 'Project', 'Contact']}
+    // showActiveTooltip={window.innerWidth > 960}
+    showActiveTooltip={true}
+    controlArrows={false}
+    continuousVertical={true}
 
-const docLinkStyle = {
-  ...linkStyle,
-  listStyleType: "none",
-  marginBottom: 24,
-}
+    render={({ state, fullpageApi }) => {
+      return (
+        <ReactFullpage.Wrapper>
+          <div className="section" id="s1">
+            <div className="content center">
+              <h1 style={{width: 'fit-content', margin: 'auto'}}>
+                <span className="emoji" role="img" aria-label="smile emoji">😄</span> Hey, I'm Josh.
+              </h1>
+            </div>
+          </div>
 
-const descriptionStyle = {
-  color: "#232129",
-  fontSize: 14,
-  marginTop: 10,
-  marginBottom: 0,
-  lineHeight: 1.25,
-}
+          {/* About Me */}
+          <div className="section" id="s2">
+            <div className="content">
+              <h1 className="center">About Me</h1>
+              <p>I am a small-town kid, born and raised in South Carolina <span className="emoji" role="img" aria-label="SC emojis">🌙🌴</span>. I am currently a junior at Duke, where I am studying Computer Science, Linguistics, & German. Go Blue Devils <span className="emoji" role="img" aria-label="blue devil emojis">🔵😈</span>!</p>
+              <p>I am passionate about the use of tech for social good. My specific interests include using tech to transform educational spaces and to overcome linguistic barriers.</p>
+              <p>I am currently a software engineer intern at <a href="https://www.lbl.gov" target="_blank" rel="noreferrer">Lawrence Berkeley National Lab</a> <span className="emoji" role="img" aria-label="national lab emojis">🌇🇺🇸🧪</span>and <a href="https://www.nersc.gov" target="_blank" rel="noreferrer">NERSC</a>. My work focuses on developing software with Javascript and Python <span className="emoji" role="img" aria-label="javascript and python emojis">☕️🐍</span> to make the supercomputers at Berkeley more accessible and user friendly through Jupyter.</p>
+            </div>
+          </div>
 
-const docLink = {
-  text: "Documentation",
-  url: "https://www.gatsbyjs.com/docs/",
-  color: "#8954A8",
-}
+          {/* Projects */}
+          <div className="section" id="s3">
+            <div className="slide">
+              <div className="content">
+                <h1 className="center">Projects</h1>
+                <h2>NERSC Dask Launcher</h2>
+                <p>Not every scientist at Berkeley Lab is a command line wizard, so I developed a frontend Jupyter widget for launching Dask distributed computing clusters on NERSC's Cori supercomputer without the need for using the terminal. Users can instead specify their launch arguments in a simple form widget that then automatically handles the resource allocation on Cori and the configuration of the Dask client.</p>
+                <p><b>Technologies: </b>Python, Dask, Bash, JupyterLab</p>
+                <div style={{display: 'inline-block'}}>
+                <a style={{float: 'left'}} href="">Prev</a>
+                <a style={{float: 'right'}} href="#section3/1">Next</a>
+                </div>
+              </div>
+            </div>
+            <div className="slide">
+              <div className="content">
+                <h1 className="center">Projects</h1>
+                <p>slide 2</p>
+                <a href="#section3/0">Prev</a>
+                <a href="">Next</a>
+              </div>
+            </div>
+          </div>
 
-const badgeStyle = {
-  color: "#fff",
-  backgroundColor: "#088413",
-  border: "1px solid #088413",
-  fontSize: 11,
-  fontWeight: "bold",
-  letterSpacing: 1,
-  borderRadius: 4,
-  padding: "4px 6px",
-  display: "inline-block",
-  position: "relative",
-  top: -2,
-  marginLeft: 10,
-  lineHeight: 1,
-}
+          {/* Contact */}
+          <div className="section" id="s4">
+            <div className="content">
+              <h1 className="center">Contact</h1>
+              <p>The best way to contact me is to email me at <a href="mailto:joshua.geden@duke.edu">joshua.geden@duke.edu</a> or to message me on LinkedIn.</p>
+            </div>
+          </div>
+        </ReactFullpage.Wrapper>
+      );
+    }}
+  />
+);
 
-// data
-const links = [
-  {
-    text: "Tutorial",
-    url: "https://www.gatsbyjs.com/docs/tutorial/",
-    description:
-      "A great place to get started if you're new to web development. Designed to guide you through setting up your first Gatsby site.",
-    color: "#E95800",
-  },
-  {
-    text: "How to Guides",
-    url: "https://www.gatsbyjs.com/docs/how-to/",
-    description:
-      "Practical step-by-step guides to help you achieve a specific goal. Most useful when you're trying to get something done.",
-    color: "#1099A8",
-  },
-  {
-    text: "Reference Guides",
-    url: "https://www.gatsbyjs.com/docs/reference/",
-    description:
-      "Nitty-gritty technical descriptions of how Gatsby works. Most useful when you need detailed information about Gatsby's APIs.",
-    color: "#BC027F",
-  },
-  {
-    text: "Conceptual Guides",
-    url: "https://www.gatsbyjs.com/docs/conceptual/",
-    description:
-      "Big-picture explanations of higher-level Gatsby concepts. Most useful for building understanding of a particular topic.",
-    color: "#0D96F2",
-  },
-  {
-    text: "Plugin Library",
-    url: "https://www.gatsbyjs.com/plugins",
-    description:
-      "Add functionality and customize your Gatsby site or app with thousands of plugins built by our amazing developer community.",
-    color: "#8EB814",
-  },
-  {
-    text: "Build and Host",
-    url: "https://www.gatsbyjs.com/cloud",
-    badge: true,
-    description:
-      "Now you’re ready to show the world! Give your Gatsby site superpowers: Build and host on Gatsby Cloud. Get started for free!",
-    color: "#663399",
-  },
-]
-
-// markup
 const IndexPage = () => {
   return (
-    <main style={pageStyles}>
-      <title>Home Page</title>
-      <h1 style={headingStyles}>
-        Congratulations
-        <br />
-        <span style={headingAccentStyles}>— you just made a Gatsby site! </span>
-        <span role="img" aria-label="Party popper emojis">
-          🎉🎉🎉
-        </span>
-      </h1>
-      <p style={paragraphStyles}>
-        Edit <code style={codeStyles}>src/pages/index.js</code> to see this page
-        update in real-time.{" "}
-        <span role="img" aria-label="Sunglasses smiley emoji">
-          😎
-        </span>
-      </p>
-      <ul style={listStyles}>
-        <li style={docLinkStyle}>
-          <a
-            style={linkStyle}
-            href={`${docLink.url}?utm_source=starter&utm_medium=start-page&utm_campaign=minimal-starter`}
-          >
-            {docLink.text}
-          </a>
-        </li>
-        {links.map(link => (
-          <li key={link.url} style={{ ...listItemStyles, color: link.color }}>
-            <span>
-              <a
-                style={linkStyle}
-                href={`${link.url}?utm_source=starter&utm_medium=start-page&utm_campaign=minimal-starter`}
-              >
-                {link.text}
-              </a>
-              {link.badge && (
-                <span style={badgeStyle} aria-label="New Badge">
-                  NEW!
-                </span>
-              )}
-              <p style={descriptionStyle}>{link.description}</p>
-            </span>
-          </li>
-        ))}
-      </ul>
-      <img
-        alt="Gatsby G Logo"
-        src="data:image/svg+xml,%3Csvg width='24' height='24' fill='none' xmlns='http://www.w3.org/2000/svg'%3E%3Cpath d='M12 2a10 10 0 110 20 10 10 0 010-20zm0 2c-3.73 0-6.86 2.55-7.75 6L14 19.75c3.45-.89 6-4.02 6-7.75h-5.25v1.5h3.45a6.37 6.37 0 01-3.89 4.44L6.06 9.69C7 7.31 9.3 5.63 12 5.63c2.13 0 4 1.04 5.18 2.65l1.23-1.06A7.959 7.959 0 0012 4zm-8 8a8 8 0 008 8c.04 0 .09 0-8-8z' fill='%23639'/%3E%3C/svg%3E"
-      />
-    </main>
+    <div>
+      {Body()}
+      {/* Footer */}
+      <div id="footer">
+        <a href="assets/Joshua_Geden_Resume.pdf" target="_blank" rel="noreferrer">Resume</a>
+        <a href="https://linkedin.com/in/joshua-geden" target="_blank" rel="noreferrer">LinkedIn</a>
+        <a href="https://github.com/Josh0823" target="_blank" rel="noreferrer">GitHub</a>
+      </div>
+    </div>
   )
 }
 
